616ee88e70e101a8530796f478998e6d985766db
RafaelAlvesoliveira <faelalvesoliveira06@gmail.com>
2022-03-22 22:31:15 -0300

    Primeira aula sobre como fazer comentários em python

---
+++ PYTHON_BASICO/AULA1/aula1.py
@@ -0,0 +1,14 @@
+# Aula sobre como fazer comentários em python
+
+print('Linha 1')  # Sempre dar dois espaços para fazer comentários em linhas seguindo as
+print('Linha 2')  # recomendações seguindo as recomendações do PEP
+print('Linha 3')
+""""    
+print('Linha 4')  #Comentários multilinhas
+print('Linha 5')  #Nesse caso são docs strings que são usadas para documentação
+print('Linha 6')
+"""
+
+'''
+Algum texto  #Aspas simples também podem ser usadas para
+'''
