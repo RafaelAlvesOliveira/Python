f6e559a3070ce4025ee685e380765aba8e49ab77
RafaelALvesOliveira <faelalvesoliveira06@gmail.com>
2023-01-26 11:53:54 -0300

    Exemplos de tipos de dados, operadores aritméticos, variáveis e f-strings

---
+++ PYTHON_S3_LOGICA/AULA26.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print("Adição", adicao)
+
+subracao = 10 - 5
+print("Subtracao", subracao)
+
+multiplicacao = 10 * 10
+print("Multiplicacao", multiplicacao)
+
+divisao = 10 / 3  # float
+print("Divisão", divisao)
+
+divisao_inteira = 10 // 3
+print("Divisão interna", divisao_inteira)
+
+exponenciacao = 2**10
+print("Exponenciação", exponenciacao)
+
+modulo = 25 % 5  # resto da divisão
+print("Módulo", modulo)
