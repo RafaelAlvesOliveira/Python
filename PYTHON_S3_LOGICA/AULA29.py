82c9ad739d512992e4ff405adc97f70481f94b93
RafaelAlvesOliveira <78687644+RafaelAlvesOliveira@users.noreply.github.com>
2023-01-30 17:59:30 -0300

    Delete PYTHON_S3_LOGICA directory

--- PYTHON_S3_LOGICA/AULA29.py
+++
@@ -1,8 +0,0 @@
-nome = 'Rafael Alves'
-altura = 1.75
-peso = 76
-
-imc = peso / (altura ** 2)
-print(nome + 'tem' + str(altura),) 
-print('pesa' + str(peso) + ' quilos e seu IMC é ')
-print(str(imc))
