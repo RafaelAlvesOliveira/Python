ce4f0ab05d14b02a14be9aed0e85203ecef61e59
RafaelALvesOliveira <faelalvesoliveira06@gmail.com>
2023-02-25 11:26:16 -0300

    Exemplos de usos de dicionários e métodos que podem ser utilizados neles, closure, High Order Function, args, empacotamento e desempacotamento, return de funções e escopo local e global.

---
+++ PYTHON_S4_INTERMEDIARIO/AULA120.py
@@ -0,0 +1,32 @@
+# Manipulando chaves e valores em dicionários
+
+pessoa = {}
+
+
+chave = 'nome'
+
+pessoa[chave] = 'Rafael'
+pessoa['sobrenome'] = 'Alves'
+
+print(pessoa[chave])
+
+pessoa[chave] = 'Maria'
+
+# del pessoa['sobrenome']
+print(pessoa)
+print(pessoa['nome'])
+
+# print(pessoa['sobrenome'])
+
+# print(pessoa.get('sobrenome'))
+
+# if pessoa['sobrenome']:
+if pessoa.get('sobrenome') is None:
+    print('Não existe')
+else:
+    print(pessoa['sobrenome'])
+# print('Isso não vai')
+
+# Não é possível acessar uma chave que não existe
+# no dicionário, mas é possível criar uma chave
+# com o sinal de atribuição.
